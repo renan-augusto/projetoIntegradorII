c73bfb4bce400a7e626551d0e6658a8210e33b6f
renan-augusto <renan_acm1992@hotmail.com>
2023-11-08 06:41:05 -0300

    dependecy injection api

---
+++ ProjetoIntegradorII.Infra.Ioc/DependecyInjectionAPI.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjetoIntegradorII.Application.Mappings;
+using ProjetoIntegradorII.Domain.Interfaces;
+using ProjetoIntegradorII.Infra.Data.Context;
+using ProjetoIntegradorII.Infra.Data.Repositories;
+
+namespace ProjetoIntegradorII.Infra.Ioc
+{
+    public static class DependecyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IAdressRepository, AdressRepository>();
+            services.AddScoped<IBeneficiaryRepository, BeneficiaryRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddScoped<ITitleRepository, TitleRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myhandlers);
+
+            return services;
+        }
+    }
+}
