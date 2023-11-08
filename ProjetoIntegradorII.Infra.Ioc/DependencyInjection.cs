298ee7c3779fa40a74d2b8b3c3dd2cdb08b3fc02
renan-augusto <renan_acm1992@hotmail.com>
2023-11-08 06:34:31 -0300

    dependency injection infra ioc configs

--- ProjetoIntegradorII.Infra.Ioc/DependencyInjection.cs
+++ ProjetoIntegradorII.Infra.Ioc/DependencyInjection.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProjetoIntegradorII.Application.Mappings;
+using ProjetoIntegradorII.Domain.Interfaces;
 using ProjetoIntegradorII.Infra.Data.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ProjetoIntegradorII.Infra.Data.Repositories;
 
 namespace ProjetoIntegradorII.Infra.Ioc
 {
@@ -18,8 +17,15 @@ namespace ProjetoIntegradorII.Infra.Ioc
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
-            //services.AddScoped<ICategoryRepository, CategoryRepository>();
-            //services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IAdressRepository, AdressRepository>();
+            services.AddScoped<IBeneficiaryRepository, BeneficiaryRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddScoped<ITitleRepository, TitleRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myhandlers);
 
             return services;
         }
