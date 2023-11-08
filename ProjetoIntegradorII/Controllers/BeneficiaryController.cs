602557f4ae153ffa48012cd75e63a34698988db3
renan-augusto <renan_acm1992@hotmail.com>
2023-11-08 07:05:22 -0300

    construction: inicio da construcao das controllers

---
+++ ProjetoIntegradorII/Controllers/BeneficiaryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoIntegradorII.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeneficiaryController : ControllerBase
+    {
+    }
+}
